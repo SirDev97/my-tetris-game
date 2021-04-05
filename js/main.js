03f7f1d1dca898504ef10684220c73ea17e510e2
Alkin Maystorov <alkin971@gmail.com>
2021-04-05 21:27:07 +0200

    Added drawSquare and drawBoard methods

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
+let square;
 function setup() {
     let canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
     canvas.parent('canvas');
+    square = new Square(60, 60, SQAURE_SIZE, SQAURE_SIZE, 'yellow');
+}
+
+function draw() {
     background(150);
+    square.drawSquare();
+    square.drawBoard();
 }
